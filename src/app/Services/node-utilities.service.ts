6201eb0a1474e837744475e610714320ea627898
Hendricks-3D <odainbrown111@gmail.com>
2020-04-06 22:23:55 -0500

    home page responsiveness fixed

--- src/app/Services/node-utilities.service.ts
+++ src/app/Services/node-utilities.service.ts
@@ -5,7 +5,6 @@
 
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders ,HttpErrorResponse, } from '@angular/common/http'
-import { Observable } from 'rxjs';
 import { IUser } from '../Interface/iuser';
 
 
